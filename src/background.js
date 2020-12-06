5e7ba9d497f586056e801f5246297a0134259940
jhn06046 <jhn06046@mju.ac.kr>
2020-12-06 16:31:03 +0900

    모바일 적용

--- src/background.js
+++ src/background.js
@@ -2,18 +2,45 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 10;
 
+let rand = 0;
+
 // 배경을 채워주는 함수
 function paintImage(imgNumber){
     const image = new Image();
     image.src = `images/${imgNumber}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
-    console.log("1");
+}
+
+function paintMobileImage(imgNumber){
+    const image = new Image();
+    smallImg = parseInt(imgNumber) + 10;
+    image.src = `images/${smallImg}.jpg`;
+    //image.src = "images/11.jpg";
+    console.log("dd");
+    image.classList.add("bgImage");
+    body.appendChild(image);
+}
+
+function checkWindow(){
+    if(window.innerWidth <= 700){
+        window.resizeTo(600,350);
+        paintMobileImage(rand);
+    }
+    else{
+        paintImage(rand);
+    }
+}
+
+window.onload = ()=>{
+    window.addEventListener('resize',()=>{
+        checkWindow();    
+    })
 }
 
 function init(){
-    const rand = Math.ceil(Math.random() * IMG_NUMBER)
-    paintImage(rand);
+    rand = Math.ceil(Math.random() * IMG_NUMBER)
+    checkWindow();
 }
 
 init();
